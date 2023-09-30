2552554f7e7730974a7849c2caa7f28812e56697
Ihor <ihor.cherednychenko@nure.ua>
2023-09-30 21:40:05 +0300

    Do x + b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int x = 1;
+        int b = 0;
+        System.out.println("Our x + b = " + (x + b));
     }
 }
