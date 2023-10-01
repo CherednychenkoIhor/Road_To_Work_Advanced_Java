71324e3615af5ff03eed540dca8b6291ed15f717
Ihor <ihor.cherednychenko@nure.ua>
2023-10-01 18:53:48 +0300

    Advanced Java: Dynamic Array (ArrayList) - How does it work? (methods: remove, add, get etc.)

---
+++ src/ArrayList/RealizationArrayList.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RealizationArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(100);
+
+    }
+}
