a4c7380470f50326b004b2e10724b2dbd4c472fa
Ihor <ihor.cherednychenko@nure.ua>
2023-10-02 21:10:28 +0300

    LinkedList - How does it work? MyLinkedList

---
+++ src/LinkedList/HowItWork.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HowItWork {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(5);
+
+        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.get(2));
+
+        myLinkedList.remove(1);
+        System.out.println(myLinkedList);
+        myLinkedList.remove(1);
+        System.out.println(myLinkedList);
+    }
+}
