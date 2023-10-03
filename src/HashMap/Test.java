ec0f7215161aefe5201d316b3b89aa7e1ea43acb
Ihor <ihor.cherednychenko@nure.ua>
2023-10-03 19:22:48 +0300

    HashMap - Introducing

---
+++ src/HashMap/Test.java
@@ -0,0 +1,28 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        // keys -> value
+        // 1 -> [... ... ]
+        // 2 -> [... ... ]
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "Один");
+        map.put(2, "Два");
+        map.put(3, "Три");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+//        System.out.println(map);
+//
+//        map.put(3, "Другое значение для ключа три"); // overwrite
+//
+//        System.out.println(map);
+//
+//        System.out.println(map.get(0));
+    }
+}
